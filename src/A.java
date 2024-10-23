1f6899ea9c1f1b77d317ef9de34b8f493736bc13
sheeqaaloow <maqane2016@outlook.com>
2024-10-23 16:24:55 -0700

    first commit

---
+++ src/A.java
@@ -0,0 +1,29 @@
+public class A
+{
+    public static void main(String[] args) {
+//        String name= "mohamed osman";
+//        String lastname= "osman";
+//        String finelname="guudlaawe";
+//        System.out.println(name);
+//        System.out.println(lastname);
+//       System.out.println(finelname);
+//         int num = 90;
+//        System.out.println(num);
+//        int num1 = 100;
+//        System.out.println(num1);
+//         int num = 20;
+//        final num =30;
+//        System.out.println(num);
+//         String name = "cabdifitaax computer";
+//        System.out.println("hallo " +name);
+//        String name = "mahamed osman";
+//        System.out.println("hello " + name);
+//        int name = 10;
+//        int name1 = 20;
+//        System.out.println(name1+name);
+        String firstname = "john";
+        String lastname = "doe";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+    }
+}
